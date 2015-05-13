4ca442b81f8c8f03a72c623fb11313b0f1cf0094
bachen <otori@qq.com>
2015-05-13 19:03:44 +0800

    update

---
+++ happy_number.py
@@ -0,0 +1,34 @@
+class Solution:
+	def isHappy(self,n):
+		result = []
+		flag = get_sum(n,result)
+		return flag
+		
+
+def get_sum(x,result):
+	ems = []
+	while True:
+		ems.append(x%10)
+		x = x/10
+		if x >= 10:
+			continue
+		else:
+			ems.append(x)
+			break
+	
+	add_sum = 0
+
+	for em in ems:
+		add_sum += em*em
+	print add_sum
+
+	if add_sum == 1:
+		return True
+	if add_sum in result:
+		return False
+	else:
+		result.append(add_sum)
+		return get_sum(add_sum,result)
+		
+test = Solution()
+print test.isHappy(12)
