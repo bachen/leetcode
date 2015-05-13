8844a189e97ce5d1db53bd8b18a047072561e849
bachen <otori@qq.com>
2015-05-13 19:07:31 +0800

    update

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -1,3 +1,11 @@
 class Solution:
 	def addTwoNumbers(self,l1,l2):
+		if l1 == None & l2 == None:
+			return None
+		if l1 == None:
+			return l2
+		if l2 == None:
+			return l1
+		if l1.next == None:
+
 		
