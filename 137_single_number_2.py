d1a1dad4e15cd10aac37bec86a80f1540aa89325
bachen <otori@qq.com>
2015-05-18 17:35:27 +0800

    update

---
+++ 137_single_number_2.py
@@ -0,0 +1,30 @@
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer}
+	def singleNumber(self, nums):
+		length = len(nums)
+		if length == 0:
+			return None
+		if length == 1:
+			return nums[0]
+		d = {}
+		for i in nums:
+			if d.has_key(i):
+				if d[i] == 1:
+					d[i] = 2
+				else:	
+					d.pop(i)
+			else:
+				d[i] = 1
+		return d.keys()[0]
+
+	def singleNumber1(self,nums):
+		length = len(nums)
+		if length == 0:
+			return None
+		if length == 1:
+			return nums[0]
+		x = 0
+		for i in nums:
+			x = x^i
+		return x
