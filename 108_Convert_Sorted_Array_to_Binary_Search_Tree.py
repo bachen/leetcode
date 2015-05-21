fa1d4ea79721eb0378f4f68d2ef3139c625b9f27
bachen <otori@qq.com>
2015-05-21 18:08:50 +0800

    2015 05 21

--- 108_Convert_Sorted_Array_to_Binary_Search_Tree.py
+++ 108_Convert_Sorted_Array_to_Binary_Search_Tree.py
@@ -9,4 +9,66 @@ class Solution:
     # @param {integer[]} nums
     # @return {TreeNode}
     def sortedArrayToBST(self, nums):
-        
+    	if nums == []:
+			return None
+		length = len(nums)
+		if length == 1:
+			root = TreeNode(nums[0])
+			return root
+		#nums = nums[::-1]
+		if length == 2:
+			root = TreeNode(nums[0])
+			root.left = TreeNode(nums[1])
+			return root
+		middle = length/2
+		root = TreeNode(nums[middle])
+		gettree(nums,0,middle,length-1)
+
+	def sortedArrayToBST1(self, nums):
+		if nums == []:
+			return None
+		length = len(nums)
+		if length == 1:
+			root = TreeNode(nums[0])
+			return root
+		#nums = nums[::-1]
+		if length == 2:
+			root = TreeNode(nums[0])
+			root.left = TreeNode(nums[1])
+			return root
+		root = TreeNode(nums[0])
+		root.left = TreeNode(nums[1])
+		root.right = TreeNode(nums[2])
+		old_nodelist = []
+		new_nodelist = [root]
+		row = 1
+		left = length - 3
+		if left == 0:
+			return root
+		i = 3
+		count = 2
+		while (i < length):
+			old_nodelist = new_nodelist
+			new_nodelist = []
+			if left > 2**row:
+				for j in range(2**row):
+					new_nodelist.append(TreeNode(i))
+					i += 1
+				left = left - 2**row
+				count = 2**row
+			else:
+				for j in range(left):
+					new_nodelist.append(TreeNode(i))
+					i += 1
+				count = left
+			j = 0
+			for node in old_nodelist:
+				if j > count - 1:
+					break
+				node.left = new_nodelist[j]
+				j += 1
+				if j > count - 1:
+					break
+				node.right = new_nodelist[j]
+				j += 1
+		return root
