ec156d31aafb73ef4a92058d11cdfa20eb29dab9
bachen <otori@qq.com>
2015-08-10 16:43:10 +0800

    update

--- Thread_2.py
+++ Thread_2.py
@@ -4,7 +4,7 @@ import threading,time
 usrs = [u'cc0001@163.com',u'cc0002@163.com']
 pwd = u'111111'
 
-def login1(usr,pwd):
+def login(usr,pwd):
 	dr = webdriver.Chrome()
 	dr.maximize_window()
 	dr.get("http://test.ximalaya.com/passport/login")
@@ -16,17 +16,11 @@ def login1(usr,pwd):
 	print "%s login successfully." % usr
 	dr.quit()
 
+def login1(usr,pwd):
+	login(usr,pwd)
+
 def login2(usr,pwd):
-	dr = webdriver.Chrome()
-	dr.maximize_window()
-	dr.get("http://test.ximalaya.com/passport/login")
-	time.sleep(2)
-	dr.find_element_by_id('userAccount').send_keys(usr)
-	dr.find_element_by_id('userPwd').send_keys(pwd)
-	dr.find_element_by_id('login_btn').click()
-	time.sleep(4)
-	print "%s login successfully." % usr
-	dr.quit()
+	login(usr,pwd)
 
 threads = []
 t1 = threading.Thread(target=login1,args=(usrs[1],pwd))
