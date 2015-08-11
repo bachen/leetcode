05e9fe653c793ff9ff88b442b0a41658a7a7f324
bachen <otori@qq.com>
2015-08-11 10:55:29 +0800

    update

--- 033_Search_in_Rotated_Sorted_Array.py
+++ 033_Search_in_Rotated_Sorted_Array.py
@@ -6,27 +6,42 @@ class Solution:
         length = len(nums)
         if length == 0:
         	return -1
-        if (length == 1):
-        	if (nums[0]==target):
+        if length == 1:
+        	if nums[0] == target:
         		return 0
         	else:
         		return -1
-        mid = length/2
-        if nums[mid]== target:
-        	return mid
-        index = -1
-        index = binarysearch(nums,0,mid,target)
-        index = binarysearch(nums,mid+1,length-1,target)
-        return index
+        pivot = None
+        for i in range(1,length):
+        	if (nums[i] - nums[i-1]) < 0:
+        		pivot = i
+        if pivot:
+        	tmp1 = binarysearch(nums[:pivot],target,0)
+        	tmp2 = binarysearch(nums[pivot:],target,pivot)
+        	print tmp1,tmp2
+        	if tmp1 != -1:
+        		return tmp1
+        	elif tmp2 != -1:
+        		return tmp2
+        	else:
+        		return -1
+        else:
+        	return binarysearch(nums,target,0)
 
-def binarysearch(nums,start,end,target):
-    if start == end:
+def binarysearch(nums,target,loc):
+    length = len(nums)
+    if length == 1:
+        if nums[0] == target:
+        	return loc
+        else:
+        	return -1
+    mid = length/2
+    if (target < nums[0]) | (target > nums[-1]):
     	return -1
-    mid = (end-start+1)/2
-    if nums[mid]== target:
-        return mid
-    binarysearch(nums,start,mid,target)
-    binarysearch(nums,mid+1,end,target)
+    if (nums[mid] > target):
+    	return binarysearch(nums[:mid],target,loc)    
+    else:
+    	return binarysearch(nums[mid:],target,mid+loc)	
 
 test = Solution()
-print test.search([4,5,6,7,0,1,2,3],4)
+print test.search([7,1,2,4,5],4)
--- 081_Search_in_Rotated_Sorted_Array_II.py
+++ 081_Search_in_Rotated_Sorted_Array_II.py
@@ -3,4 +3,39 @@ class Solution:
     # @param {integer} target
     # @return {boolean}
     def search(self, nums, target):
-        
+        length = len(nums)
+        if length == 0:
+        	return False
+        if length == 1:
+        	if nums[0] == target:
+        		return True
+        	else:
+        		return False
+        pivot = None
+        for i in range(1,length):
+        	if (nums[i] - nums[i-1]) < 0:
+        		pivot = i
+        if pivot:
+        	return (binarysearch(nums[:pivot],target) | binarysearch(nums[pivot:],target))
+        else:
+        	return binarysearch(nums,target)
+
+def binarysearch(nums,target):
+    length = len(nums)
+    if length == 1:
+        if nums[0] == target:
+        	return True
+        else:
+        	return False
+    mid = length/2
+    if (target < nums[0]) | (target > nums[-1]):
+    	return False
+    if (nums[mid] > target):
+    	return binarysearch(nums[:mid],target)    
+    elif nums[mid] == target:
+    	return True
+    else:
+    	return binarysearch(nums[mid:],target)	
+
+test = Solution()
+print test.search([5,7,1,2,4,5],5)
