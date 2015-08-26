2791e9dd05df76c1b3dea6d2079447b094238ebe
bachen <otori@qq.com>
2015-08-26 09:53:49 +0800

    update 268

--- 268_Missing_Number.py
+++ 268_Missing_Number.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def missingNumber(self, nums):
+    def missingNumber1(self, nums):
         """
         :type nums: List[int]
         :rtype: int
@@ -12,5 +12,26 @@ class Solution(object):
         	count += 1
         return count
 
+    def missingNumber(self, nums): #constant space, linear time
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_val = nums[0]
+        real = 0
+        ideal = 0
+        count = 0
+        for i in nums:
+        	if max_val < i:
+        		max_val = i
+        	real += i
+        	ideal += count
+        	count += 1
+        res = max_val - (real-ideal)
+        if res == max_val:
+        	return max_val+1
+        else:
+        	return res
+
 test = Solution()
-print test.missingNumber([0])
+print test.missingNumber([0,3,4,2])
