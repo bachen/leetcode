419eacf8ff62733869e3f60c24870001291bbdd7
bachen <otori@qq.com>
2015-10-29 15:17:11 +0800

    update

--- 297_Serialize_and_Deserialize_Binary_Tree.py
+++ 297_Serialize_and_Deserialize_Binary_Tree.py
@@ -1,4 +1,6 @@
 # Definition for a binary tree node.
+import Queue
+
 class TreeNode(object):
     def __init__(self, x):
         self.val = x
@@ -15,15 +17,16 @@ class Codec:
         """
         if root == None:
             return "[null]"
-        res = serial(left,right,[str(root.val)])
+        q = Queue.Queue(-1)
+        q.put(root)
+        res = serial(q,1,[])
         length = len(res)
-        if length == (2**(length/2)-1):
-            return '['+ ' '.join(res) +']'
-        else:
-            for i in range(length):
-                if res[-1] == 'null':
-                    res.pop()
-            return '['+ ' '.join(res) +']'
+        for i in xrange(-1,-length,-1):
+            if res[-1] == 'null':
+                res.pop()
+            else:
+                break
+        return '[' + ' '.join(res) + ']'
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -38,48 +41,55 @@ class Codec:
         nodes = data.split(' ')
         length = len(nodes)
         root = TreeNode(nodes[0])
-        deserial(root,nodes,1,length)
+        q = Queue.Queue(-1)
+        q.put(root)
+        deserial(q,1,nodes,1,length)
         return root
 
-def serial(left,right,s):
-    if (left == None) & (right == None):
-        s.append('null')
-        s.append('null')
-    elif (left != None) & (right == None):
-        s.append(str(left.val))
-        s.append('null')
-        serial(left.left,left.right,s)
-    elif (root.left == None) & (root.right != None):
-        s.append('null')
-        s.append(str(root.right.val))
-        serial(root.right,s)
-    else:
-        s.append(str(root.left.val))
-        s.append(str(root.right.val))
-        serial(root.left,s)
-        serial(root.right,s)
+def serial(q,size,s):
+    count = 0
+    if size == 0:
+        return s
+    for i in range(size):
+        node = q.get()
+        if node == None:
+            s.append('null')
+        else:
+            s.append(str(node.val))
+            q.put(node.left)
+            q.put(node.right)
+            count += 2
+    serial(q,count,s)
     return s
 
-def deserial(root,nodes,count,length):
-    if count >= length:
+def deserial(q,size,nodes,loc,length):
+    count = 0
+    if size == 0:
         return
-    if nodes[count] != 'null':
-        root.left = TreeNode(nodes[count])
-    count += 1
-    if count >= length:
+    if loc > length:
         return
-    if nodes[count] != 'null':
-        root.right = TreeNode(nodes[count])
-    count += 1
-    if count >= length:
-        return
-    if root.left != None:
-        deserial(root.left,nodes,count,length)
-        count += 2
-    if count >= length:
-        return
-    if root.right != None:
-        deserial(root.right,nodes,count,length)
+    for i in range(size):
+        node = q.get()
+        if node == None:
+            loc += 1
+            continue
+        else:
+            if loc < length:
+                node.left = TreeNode(nodes[loc])
+                loc += 1
+                q.put(node.left)
+                count += 1
+            else:
+                return
+            if loc < length:
+                node.right = TreeNode(nodes[loc])
+                loc += 1
+                q.put(node.right)
+                count += 1
+            else:
+                return
+    deserial(q,count,nodes,loc,length)
+
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
 # codec.deserialize(codec.serialize(root))
@@ -91,9 +101,10 @@ m[0].left = m[1]
 m[0].right = m[2]
 m[1].left = m[3]
 m[1].right = m[4]
-m[2].left = m[5]
+#m[2].left = m[5]
 m[2].right = m[6]
+m[6].right = m[7]
 
 c = Codec()
-print c.deserialize('[1 2 3 4 5 6 7 8 9 10]').left.left.left.val
-#print c.serialize(m[0])
+#print c.deserialize('[1]').val
+print c.serialize(m[0])
